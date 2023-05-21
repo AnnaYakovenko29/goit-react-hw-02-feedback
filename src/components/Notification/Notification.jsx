b10e884add8e71469410616dc147cf3cf67acc99
AnnaYakovenko29 <125920571+AnnaYakovenko29@users.noreply.github.com>
2023-05-21 22:37:07 +0300

    goit-react-hw-02-feedback

---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,4 @@
+import './Notification.css'
+export const Notification = ({ message }) => {
+    return <p className='titleNotification'>{message}</p>;
+  };
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+import './Section.css';
+
+export const Section = ({ title, children }) => {
+  return (
+    <div className='section'>
+      {title && <p className='titleSection'>{title}</p>}
+      {children}
+    </div>
+  );
+};
+
+Section.propTypes = {
+  title: PropTypes.string,
+};
